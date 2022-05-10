bb5238400cb6d90af85dfabfd909b7f123f36e90
永吉勇斗 <yokonori@yt-MBP.local>
2022-05-10 21:53:21 +0900

    category,top,create_post html/css修正

--- resources/views/category/category.blade.php
+++ resources/views/category/category.blade.php
@@ -5,24 +5,32 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="{{ asset('css/category.css') }}">
     </head>
     <body>
     @section('category')
-    <div class="container mt-3">
-        <h2>カテゴリー選択</h2>
+    <h2>カテゴリー選択</h2>
+    <div class="container-fluid">
     {{-- モーダルでボタン３つ表示、中身が違う⇨data-targetとidをそれぞれ変更する --}}
-    <!-- Button to Open the Modal -->
-        <button type="button" class="btn btn-primary" id="camp_show">
-            CAMP
-        </button>
-        <!-- The Modal -->
+        <div class="category">
+            <!-- Button to Open the Modal -->
+            <h3 class="camp">CAMP</h3>
+            <button type="button" class="btn" id="camp_show">
+                <img src="/images/category_camp.png" alt="テントと車の写真" height="250px" width="250px">
+            </button>
+        </div>
+            <!-- The Modal -->
         <div class="modal fade" id="camp" role="dialog"> 
-            <div class="modal-dialog modal-xl">
+            <div class="modal-dialog modal-dialog-centered modal-xl">
                 <div class="modal-content">
             <!-- Modal Header -->
                     <div class="modal-header">
                         <h4 class="modal-title">CAMP</h4>
-                        <button type="button" class="close" data-dismiss="modal">×</button>
+                        <button type="button" class="close" data-dismiss="modal"></button>
                     </div>
             <!-- Modal body -->
                     <div class="modal-body">
@@ -43,21 +51,24 @@
                 </div>
             </div>
         </div>
-    <!-- Button to Open the Modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#cook">
-            FOOD
-        </button>
-        {{-- toggle = 何をするか  target = どの要素を開くか---↑cookというIDをもつモーダルが表示される --}}
 
+        <div class="category">
+            <!-- Button to Open the Modal -->
+            <h3 class="food">FOOD</h3>
+            <button type="button" class="btn" data-toggle="modal" data-target="#cook">
+                <img src="/images/category_food.png" alt="料理の写真" height="250px" width="250px">
+            </button>
+            {{-- toggle = 何をするか  target = どの要素を開くか---↑cookというIDをもつモーダルが表示される --}}
+        </div>
         <!-- The Modal -->
         <div class="modal fade" id="cook" aria-labelledby="basicModal"aria-hidden="true">
                     {{-- idはdata-targetに合わせる --}}
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
             <!-- Modal Header -->
                     <div class="modal-header">
                         <h4 class="modal-title">FOOD</h4>
-                        <button type="button" class="close" data-dismiss="modal">×</button>
+                        <button type="button" class="close" data-dismiss="modal"></button>
                     </div>
             <!-- Modal body -->
                     <div class="modal-body">
@@ -86,18 +97,21 @@
             </div>
         </div>
 
-    <!-- Button to Open the Modal -->
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#gear">
-            GEAR
-        </button>
+        <div class="category">
+            <!-- Button to Open the Modal -->
+            <h3 class="gear">GEAR</h3>
+            <button type="button" class="btn" data-toggle="modal" data-target="#gear">
+                <img src="/images/category_gear.png" alt="キャンプ道具の写真" height="250px" width="250px">
+            </button>
+        </div>
         <!-- The Modal -->
         <div class="modal fade" id="gear" aria-labelledby="basicModal"aria-hidden="true">
-            <div class="modal-dialog">
+            <div class="modal-dialog modal-dialog-centered">
                 <div class="modal-content">
             <!-- Modal Header -->
                     <div class="modal-header">
                         <h4 class="modal-title">GEAR</h4>
-                        <button type="button" class="close" data-dismiss="modal">×</button>
+                        <button type="button" class="close" data-dismiss="modal"></button>
                     </div>
             <!-- Modal body -->
                     <div class="modal-body">
