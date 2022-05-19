878b134f0704d9f66ac87f84ee4c1c94aa8fe0d0
永吉勇斗 <yokonori@yt-MBP.local>
2022-05-20 06:05:47 +0900

    メール本文修正

--- resources/views/auth/emails_text.blade.php
+++ resources/views/auth/emails_text.blade.php
@@ -11,7 +11,7 @@
         
             <p>下記URLから登録を進めてください。<p>
             
-            <p>http://13.208.235.254//register/verify/{{ $data }}<p>
+            <p>http://excitecamp//register/verify/{{ $data }}<p>
         </div>
     </body>
 </html>
