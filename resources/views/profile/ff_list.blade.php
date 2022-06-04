0ea4eaf3aeef172faa6c42356f82ce5d5b82a8c7
永吉勇斗 <yokonori@yt-MBP.local>
2022-06-05 07:43:42 +0900

    ff_list修正

--- resources/views/profile/ff_list.blade.php
+++ resources/views/profile/ff_list.blade.php
@@ -1,22 +1,36 @@
 @extends('header')
 
 @section('head')
+    <link rel="stylesheet" href="{{ asset('/css/ff_list.css') }}">
 @endsection
     
 @section('body')
-    <div>
+    <div class="container">
+        <div class="switching">
+            <a href="{{ route('ff_list',['id' => Auth::id()]) }}?1=follow">フォロー</a>
+            <a href="{{ route('ff_list',['id' => Auth::id()]) }}?2=follower">フォロワー</a>
+        </div>
+        <div class="list">
         @if (request()->get('1'))
             @if($follow_users)
                 @foreach ($follow_users as $follow_user)
+                <div class="user">
                     <a href="{{ route('profile_detail',['id' => $follow_user->id]) }}">{{ $follow_user->name }}</a>
-                @endforeach
-            @endif
-        @elseif(request()->get('2'))
-            @if($follower_users)
-                @foreach ($follower_users as $follower_user)
-                    <a href="{{ route('profile_detail',['id' => $follower_user->id]) }}">{{ $follower_user->name }}</a>
+                </div>
                 @endforeach
             @endif
         @endif
+        </div>
+        <div class="list">
+        @if(request()->get('2'))
+            @if($follower_users)
+                @foreach ($follower_users as $follower_user)
+                <div class="user">
+                    <a href="{{ route('profile_detail',['id' => $follower_user->id]) }}">{{ $follower_user->name }}</a>
+                </div>
+                @endforeach
+            @endif
+        @endif
+        </div>
     </div>
 @endsection
