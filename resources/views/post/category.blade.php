715394fdef8b1cf4aad200974663a0372cf61ba4
永吉勇斗 <yokonori@yt-MBP.local>
2022-06-06 19:25:08 +0900

    不使用コード削除

--- resources/views/post/category.blade.php
+++ resources/views/post/category.blade.php
@@ -141,31 +141,4 @@
     <script src="{{ asset('/js/map.js') }}"></script>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBlZCYYOoFZOIseoW_YfdYcX5TIupEPAzI&callback=initMap"></script>
 
-
-    {{------------------ modal validation ------------------------}}
-    <script>
-        // (function() {
-        //     // 厳格モード
-        //     'use strict';
-        //         // load ページの読み込みが完了した時
-        //         window.addEventListener('load', function() {
-        //         // className 取得
-        //         const forms = document.getElementsByClassName('needs-validation');
-        //         const validation = Array.prototype.filter.call(forms, function(form) {
-        //             // submitイベントでバリデーションを実行する
-        //             form.addEventListener('submit', function(event) {
-        //             // バリデーションを実行する
-        //             if (form.checkValidity() === false) {
-        //                 // バリデーション失敗時にsubmitイベントを停止させる
-        //                 event.preventDefault();
-        //                 event.stopPropagation();
-        //             }
-        //             // バリデーション終了のCSSクラスを追加する
-        //             form.classList.add('was-validated');
-        //             }, false);
-        //         });
-        //     }, false);
-        // })();
-    </script>
-
 @endsection
